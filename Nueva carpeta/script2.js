fa2fc0ae0484d94012bc51645aff0eea32b30c6a
Jano <janod1300@gmail.com>
2021-11-22 03:38:15 -0300

    [MAKE:FILE] Agregue primer version de endpoints Clase 19

---
+++ Nueva carpeta/script2.js
@@ -0,0 +1,106 @@
+/*
+const cors = require("cors");
+const multer = require("multer");
+const  MethodOverride = require("method-override");
+const { v4: uuidv4 } = require('uuid');
+const express = require("express");
+const dayjs = require("dayjs");
+*/
+
+import cors from "cors";
+import multer from "multer";
+import MethodOverride from "method-override";
+import {
+    v4 as uuid
+} from "uuid";
+import express from "express";
+import path from "path";
+import dayjs from "dayjs";  
+import { setServers } from "dns";
+
+const server = express();
+const log = console.log;
+
+let port = process.env.PORT || 3000;
+
+server.use(express.urlencoded({extended:true}));
+server.use(express.json());
+server.use(cors());
+server.use(MethodOverride());
+
+let users = [{
+        email: "asd@gmail.com",
+        name: "asd",
+        pass: "12345678"
+    }
+
+];   
+
+server.listen(3000, () => {
+    log("start server");
+}).on("error", () => {
+    log("error server");
+});
+
+server.get("/", (req, res)=>{                                                               
+    res.sendFile(path.join(__dirname, "/index.html")); 
+});
+
+
+//1- Mostrar array de users                                              
+server.get("/users", (req, res) => {
+    res.send(users);
+});
+
+//2- Mostrar un user mediante el email             
+
+server.get("/users/email/:email", (req,res)=>{
+    let email = req.params.email;
+    res.send(users.filter(user => user.email === email));
+});
+
+//3- Mostrar varios users mediante su email            
+server.get("/users/emails/:email", (req,res)=>{
+    let email = req.params.email;
+    users.forEach(params =>{
+        res.send(users.filter(user => user.email === email));
+    })
+    
+});
+
+//4-Mostrar todos los users por su nombre               
+server.get("/mostrar/user/", (req,res)=>{
+    let name = req.query.name;
+    res.send(users.filter(user => user.name === req.query.name))
+});
+
+
+//5- Crear user, verificar que mail no exista          
+server.post("/users/create", (req,res)=>{
+    const {name, email, pass} = req.body;
+    users.push({name, email, pass});
+    res.send("Usuario agregado");
+});
+
+//6- Eliminar user por email                               
+server.delete("/users/delete/:email", (req,res)=>{
+    users = users.filter(user => user.email !== req.params.email);
+    res.send("Usuario eliminado");
+});
+
+//7-Eliminar varios users por su email                     
+server.delete("/users/delete/", (req,res)=>{
+    
+    users.forEach(params => {
+        users = users.filter(user => user.email !== req.query.email);
+    })
+    res.send("Usuarios eliminados");
+});
+
+//8- Actualizar user, lo identifica mediante su email     
+server.put("/users/actualizar", (req, res)=>{
+    const{name, email, pass} = req.body;
+    users = users.filter(user => user.email !== email);
+    users.push({name,email,pass});
+    res.send("Usuario Actualizado");
+});
